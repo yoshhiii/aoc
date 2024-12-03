c5e7bae9f870abaf2d6e9fb45926387bbc48e100
Doug Coble <13511246+yoshhiii@users.noreply.github.com>
2024-12-03 16:47:38 -0500

    day03

--- aoc2024/lib/day03/solution.exs
+++ aoc2024/lib/day03/solution.exs
@@ -1,20 +1,66 @@
 defmodule Aoc2024.Day03.Solution do
   def read_input(file_path) do
-    File.stream!(file_path)
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.split(&1, " ", trim: true))
-    |> Stream.map(fn list -> Enum.map(list, &String.to_integer/1) end)
-    |> Enum.to_list()
+    File.read!(file_path)
   end
 
   def part_one(file_path) do
-    nil
+    regex = ~r/mul\((\d+),(\d+)\)/
+
+    read_input(file_path)
+    |> then(&Regex.scan(regex, &1))
+    |> Enum.map(fn [_, first, second] ->
+      first_val = String.to_integer(first)
+      second_val = String.to_integer(second)
+      first_val * second_val
+    end)
+    |> Enum.sum()
+    |> IO.inspect()
+  end
+
+  def extract_between(input, start_keyword, end_keyword, content_pattern) do
+    input
+    |> String.split(start_keyword)
+    |> Enum.drop(1)
+    |> Enum.map(&String.split(&1, end_keyword))
+    |> Enum.map(&List.first/1)
+    |> Enum.flat_map(fn content ->
+      Regex.scan(content_pattern, content || "")
+      |> Enum.map(fn [_, a, b] ->
+        String.to_integer(a) * String.to_integer(b)
+      end)
+    end)
+    |> Enum.sum()
   end
 
   def part_two(file_path) do
-    nil
+    file = read_input(file_path)
+
+    start_keyword = "do()"
+    end_keyword = "don't()"
+    mul_regex = ~r/mul\((\d+),(\d+)\)/
+
+    first_keyword_pos =
+      case Regex.run(~r/do\(\)|don't/, file, return: :index) do
+        nil -> String.length(file)
+        [{pos, _}] -> pos
+      end
+
+    before_section = String.slice(file, 0, first_keyword_pos)
+
+    muls_before_keyword =
+      Regex.scan(mul_regex, before_section)
+      |> Enum.map(fn [_, a, b] ->
+        String.to_integer(a) * String.to_integer(b)
+      end)
+      |> Enum.sum()
+
+    matches =
+      Aoc2024.Day03.Solution.extract_between(file, start_keyword, end_keyword, mul_regex)
+
+    (matches + muls_before_keyword)
+    |> IO.inspect()
   end
 end
 
-Aoc2024.Day03.Solution.part_one("lib/day03/input_test")
-# Aoc2024.Day03.Solution.part_two("lib/day03/input")
+# Aoc2024.Day03.Solution.part_one("lib/day03/input")
+Aoc2024.Day03.Solution.part_two("lib/day03/input")
