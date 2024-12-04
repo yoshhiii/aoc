5f1ddb25b8e231a23b03252334d1133b19a0770f
Doug Coble <13511246+yoshhiii@users.noreply.github.com>
2024-12-04 16:51:59 -0500

    day04 part two

--- fsharp2024/day04/solution.fsx
+++ fsharp2024/day04/solution.fsx
@@ -1,6 +1,5 @@
 open System
 open System.IO
-open System.Collections.Generic
 
 let cardinalDirections = [ (-1, 0); (1, 0); (0, -1); (0, 1) ]
 let ordinalDirections = [ (-1, -1); (1, -1); (-1, 1); (1, 1) ]
@@ -10,65 +9,105 @@ type Direction =
     | Ordinal
     | All
 
-let isValid (grid: char[,]) (row: int) (col: int) =
-    row >= 0 && row < grid.GetLength(0) && col >= 0 && col < grid.GetLength(1)
-
-let search (grid: char[,]) (keyword: string) (searchDir: Direction) (startRow: int) (startCol: int) =
+let search
+    (grid: char[,])
+    (keyword: string)
+    (searchDir: Direction)
+    (startRow: int)
+    (startCol: int)
+    (startChar: Nullable<char>)
+    =
     let rows = grid.GetLength 0
     let cols = grid.GetLength 1
     let keywordLength = keyword.Length
-    let mutable keywordCount = 0
 
-    let directions = 
+    let directions =
         match searchDir with
-            | Direction.Cardinal -> cardinalDirections
-            | Direction.Ordinal -> ordinalDirections
-            | Direction.All -> cardinalDirections @ ordinalDirections
+        | Direction.Cardinal -> cardinalDirections
+        | Direction.Ordinal -> ordinalDirections
+        | Direction.All -> cardinalDirections @ ordinalDirections
 
-    let rec searchDirection (row: int) (col: int) (dr: int) (dc: int) (index: int) =
-        if index = keywordLength then
-            true
-        elif not (isValid grid row col) || grid.[row, col] <> keyword.[index] then
-            false
+    let isValid (row: int) (col: int) =
+        row >= 0 && row < rows && col >= 0 && col < cols
+
+    let searchDirection (row: int) (col: int) (dr: int) (dc: int) =
+        let rec loop (r: int) (c: int) (index: int) =
+            if index = keywordLength then
+                true
+            elif not (isValid r c) || grid.[r, c] <> keyword.[index] then
+                false
+            else
+                loop (r + dr) (c + dc) (index + 1)
+
+        loop row col 0
+
+    let findXPattern (i: int) (j: int) =
+        let window = Array2D.init 3 3 (fun x y -> grid.[i + x, j + y])
+        let topLeft = window.[0, 0]
+        let topRight = window.[0, 2]
+        let bottomLeft = window.[2, 0]
+        let bottomRight = window.[2, 2]
+
+        let patterns =
+            [ (topLeft, bottomRight, topRight, bottomLeft)
+              (topLeft, bottomRight, bottomLeft, topRight)
+              (bottomLeft, topRight, topLeft, bottomRight)
+              (bottomLeft, topRight, bottomRight, topLeft) ]
+
+        patterns
+        |> List.exists (fun (tl, br, tr, bl) ->
+            tl = 'M' && br = 'S' && tr = 'M' && bl = 'S'
+            || tl = 'M' && br = 'S' && tr = 'S' && bl = 'M'
+            || tl = 'S' && br = 'M' && tr = 'S' && bl = 'M'
+            || tl = 'S' && br = 'M' && tr = 'M' && bl = 'S')
+
+    let keywordCount =
+        if startChar.HasValue then
+            [ for i in 0 .. rows - 3 do
+                  for j in 0 .. cols - 3 do
+                      if grid.[i + 1, j + 1] = startChar.Value && findXPattern i j then
+                          yield 1 ]
+            |> List.sum
         else
-            searchDirection (row + dr) (col + dc) dr dc (index + 1)
+            let visited = Array2D.create rows cols false
+            let queue = System.Collections.Generic.Queue<(int * int)>()
+            queue.Enqueue((startRow, startCol))
+            visited.[startRow, startCol] <- true
 
-    let visited = Array2D.create rows cols false
-    let queue = Queue<(int * int)>()
+            let rec bfs count =
+                if queue.Count = 0 then
+                    count
+                else
+                    let (currentRow, currentCol) = queue.Dequeue()
 
-    queue.Enqueue((startRow, startCol))
-    visited.[startRow, startCol] <- true
+                    let newCount =
+                        directions
+                        |> List.fold
+                            (fun acc (dr, dc) ->
+                                if searchDirection currentRow currentCol dr dc then
+                                    acc + 1
+                                else
+                                    acc)
+                            count
 
-    while queue.Count > 0 do
-        let (currentRow, currentCol) = queue.Dequeue()
-        printfn "Visiting cell (%d, %d)" currentRow currentCol
+                    directions
+                    |> List.iter (fun (dr, dc) ->
+                        let newRow = currentRow + dr
+                        let newCol = currentCol + dc
 
-        for (dr, dc) in directions do
-            if searchDirection currentRow currentCol dr dc 0 then
-                printfn
-                    "Keyword '%s' found starting at (%d, %d) in direction (%d, %d)"
-                    keyword
-                    currentRow
-                    currentCol
-                    dr
-                    dc
+                        if isValid newRow newCol && not visited.[newRow, newCol] then
+                            queue.Enqueue((newRow, newCol))
+                            visited.[newRow, newCol] <- true)
 
-                keywordCount <- keywordCount + 1
+                    bfs newCount
 
-            let newRow = currentRow + dr
-            let newCol = currentCol + dc
-
-            if isValid grid newRow newCol && not visited.[newRow, newCol] then
-                visited.[newRow, newCol] <- true
-                queue.Enqueue((newRow, newCol))
+            bfs 0
 
     keywordCount
 
-let createGrid (data : array<string>) =
-    let charArr =
-        data 
-        |> Array.map(fun s -> s.ToCharArray())
-        
+let createGrid (data: array<string>) =
+    let charArr = data |> Array.map (fun s -> s.ToCharArray())
+
     let rows = charArr.Length
     let cols = charArr.[0].Length
     Array2D.init rows cols (fun i j -> data.[i].[j])
@@ -77,15 +116,15 @@ let partOne (filePath: string) =
     let grid = File.ReadAllLines filePath |> createGrid
 
     let keyword = "XMAS"
-    let count = search grid keyword Direction.All 0 0
+    let count = search grid keyword Direction.All 0 0 (Nullable())
     printfn "%s found %d times" keyword count
 
-let partTwo (filePath: string) = 
+let partTwo (filePath: string) =
     let grid = File.ReadAllLines filePath |> createGrid
 
     let keyword = "MAS"
-    let count = search grid keyword Direction.Ordinal 0 0
+    let count = search grid keyword Direction.Ordinal 0 0 (Nullable 'A')
     printfn "%s found %d times" keyword count
 
-// partOne "./input"
-partTwo "./input_test"
+partOne "./input"
+partTwo "./input"
